bbb1f7bb567cdcf643cbc383c676d3d6f684fd9a
Ksenia <ksenia_s.a@bk.ru>
2023-06-22 01:50:16 +0700

    fix 2 issue

--- sudoku.py
+++ sudoku.py
@@ -10,15 +10,11 @@ from copy import deepcopy
 
 game_won = False
 
-## Read sudoku data
 f = open('sudoku.csv')
 text = f.read()
 lines = text.splitlines()
 
 # Get one of the puzzles and its corresponding solution
-# line_number = 20
-# line_number = 1
-# line_number = 600
 line_number = random.randint(0, len(lines))
 line = lines[line_number]
 line = line.strip()
@@ -47,10 +43,8 @@ print("""
 
 ## Fill Board with puzzle data
 spots = iter(puzzle)
-# change from a string to a list of list of ints
 puzzle_board = [[int(next(spots)) for _ in range(9)] for _ in range(9)] 
 solution_spots = iter(solution)
-# change form a string to a list of list of ints
 solution_board = [[int(next(solution_spots)) for _ in range(9)] for _ in range(9)]  
 
 print("""
@@ -82,10 +76,9 @@ print("""
 {} {} {} | {} {} {} | {} {} {}
 """.format(*[val if val else ' ' for row in solution_board for val in row]))
 
-## Check if the board is valid.
+## Check if the board is valid
 def rowsValid(board):
     for row in board:
-        # check if the values are all unique, not counting zeroes
         if len([val for val in row if val]) == len(set(val for val in row if val)):  
             continue
         else:
@@ -97,9 +90,7 @@ print(rowsValid(solution_board))
 def cols_vald(board):
     is_valid = []
     for col in zip(*board):
-        # check if the column contains unique values, not counting zeroes
-        is_valid.append(len(list(filter(bool, col))) == len(set(filter(bool, col))))
-    # if all the columns are valid   
+        is_valid.append(len(list(filter(bool, col))) == len(set(filter(bool, col))))  
     if all(is_valid):  
         return True
     else:
@@ -138,7 +129,6 @@ print(box_valid(solution_board))
 pyxel.init(156, 183, title="Sudoku Game")
 
 def board_valid(problem_board, solution_board):
-    # all the rules of sudoku haven't been broken
     if rowsValid(problem_board):
         if cols_vald(problem_board):
             if box_valid(problem_board):
@@ -149,7 +139,6 @@ def board_valid(problem_board, solution_board):
                 return False
     else:
                 return False
-    # the board matches the solution board so far.
     for pcol, scol in zip(problem_board, solution_board):
         for pval, sval in zip(pcol, scol):
             if pval:
@@ -214,19 +203,17 @@ def draw():
     global cell_selected
     global selected_value
     global game_won
-    
+
     if game_won:
         # Make bakcground yellow if game is won
         pyxel.cls(10)  
-    # Draw each space
     for i, row in enumerate(puzzle_board):
         for j, value in enumerate(row):
             x_offset = 2
             y_offset = 2
             # where to put the subimage
-            x = i * 16 + i + x_offset
-            # where to put the subimage
-            y = j * 16 + j + y_offset
+            x = i*16 + i + x_offset
+            y = j*16 + j + y_offset
             image_size = 16
             w = image_size
             h = image_size
@@ -236,22 +223,21 @@ def draw():
                 transparent_color = 7
             else:
                 transparent_color = 10
-            # copy part of image from resource file to the screen.
+            # copy part of image from resource file to the screen
             pyxel.blt(x, y, 0, u, v, w, h, transparent_color)  
     # Draw the lines of the board
     lines_col = 0
-    pyxel.rect(0 + x_offset, 50 + y_offset, w = 16 * 9 + 8, h=1, col=lines_col)
-    pyxel.rect(0 + x_offset, 101 + y_offset, w=16 * 9 + 8, h=1, col=lines_col)
-    pyxel.rect(50 + x_offset, 0 + y_offset, h=16 *9 +8, w=1, col=lines_col)
-    pyxel.rect(101 + x_offset, 0 + y_offset, h=16 * 9+ 8, w=1, col=lines_col)
+    pyxel.rect(0 + x_offset, 50 + y_offset, w=16*9 + 8, h=1, col=lines_col)
+    pyxel.rect(0 + x_offset, 101 + y_offset, w=16*9 + 8, h=1, col=lines_col)
+    pyxel.rect(50 + x_offset, 0 + y_offset, h=16*9 +8, w=1, col=lines_col)
+    pyxel.rect(101 + x_offset, 0 + y_offset, h=16*9+ 8, w=1, col=lines_col)
     pyxel.rect(0, 156, h=7, w=200, col=0)
     for idx in range(9):
         if selected_value == idx + 1:
             transparent_color = 7
         else:
             transparent_color = 10
-        #copy part of image from resource file to the screen.
-        pyxel.blt(idx * 16 + idx + x_offset, 165, 0, u=0, v = (idx + 1) * 16, w=image_size, h=image_size, colkey=transparent_color)  
+        pyxel.blt(idx*16 + idx + x_offset, 165, 0, u=0, v = (idx+1) * 16, w=image_size, h=image_size, colkey=transparent_color)  
 
 def get_board_spot(mouse_x, mouse_y):
     return min(int(mouse_x // 17), 8), min(int(mouse_y // 17), 8)
@@ -263,7 +249,7 @@ def update():
     global cell_selected
     global selected_value
     global game_won
-    
+
     if pyxel.btnp(pyxel.KEY_Q):
         pyxel.quit()
     # select the board spot when the player clicks the left mouse button
@@ -272,9 +258,9 @@ def update():
         print(mouse_pos)
         board_spot = get_board_spot(*mouse_pos)
         if mouse_pos[1] < 155:
-            cell_selected=board_spot
+            cell_selected = board_spot
         else:
-            selected_value=board_spot[0]+1
+            selected_value = board_spot[0] + 1
             print('selected value:',selected_value)
     # update the board spot when the player clicks the right mouse button
     if pyxel.btnp(1):
@@ -292,9 +278,9 @@ def update():
         print('full')
         if board_valid(puzzle_board):
             print('hi')
-            game_won=True
+            game_won = True
         else:
-            game_won=False
+            game_won = False
     else:
         game_won = False
 
@@ -306,7 +292,6 @@ def board_is_full(board):
             else:
                 return True
 
-###start the game###
 pyxel.run(update, draw)
 print("That was fun, why don't we play again?")
 
