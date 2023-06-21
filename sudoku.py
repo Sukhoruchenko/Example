bd065bf711b45dbefa775716129824414a43572a
Ksenia <ksenia_s.a@bk.ru>
2023-06-22 06:00:48 +0700

    fix 9, 11, 12 issues

--- sudoku.py
+++ sudoku.py
@@ -69,7 +69,6 @@ def box_valid(board):
     return True
 
 print(box_valid(solution_board))
-
 width_size = 156
 height_size = 183
 pyxel.init(width_size, height_size, title="Sudoku Game")
@@ -93,53 +92,78 @@ def update_board(board, row, col, value):
     return new_board
 
 cell_coordinates = (0, 0)
-
 print(board.format(*[unit if unit else ' ' for row in puzzle_board for unit in row]))
 print(board.format(*[unit if unit else ' ' for row in update_board(puzzle_board, 4, 4, 8) for unit in row]))
-
 pyxel.cls(col=3)
 print(pyxel.load('my_resource.pyxres', True, True))
 image = pyxel.image(0)
 pyxel.mouse(True)
             
-def draw():
+def draw_cells():
     global puzzle_board
-    global solution_board
     global cell_coordinates
     global selected_value
-    global game_won
 
-    if game_won:
-        # Make bakcground yellow if game is won
-        pyxel.cls(col=10)  
+    x_offset = 2
+    y_offset = 2
+    image_size = 16
     for i, row in enumerate(puzzle_board):
         for j, value in enumerate(row):
-            x_offset = 2
-            y_offset = 2
-            # where to put the subimage
-            image_size = 16
             x = i*image_size + i + x_offset
             y = j*image_size + j + y_offset
             u_width = 0
             v_height = value * image_size
-            if cell_coordinates == (i, j):
-                transparent_color = 5
-            else:
-                transparent_color = 10
-            # copy part of image from resource file to the screen
-            pyxel.blt(x, y, 0, u_width, v_height, image_size, image_size, transparent_color)  
-    # Draw the lines of the board
+            transparent_color = get_cell_transparent_color((i, j))
+            pyxel.blt(x, y, 0, u_width, v_height, image_size, image_size, transparent_color)
+
+def get_cell_transparent_color(cell_coordinate):
+    global cell_coordinates
+    global selected_value
+
+    if cell_coordinate == cell_coordinates:
+        transparent_color = 5
+    elif selected_value and puzzle_board[cell_coordinate[0]][cell_coordinate[1]] == 0:
+        transparent_color = 10
+    else:
+        transparent_color = 0
+    return transparent_color
+
+def draw_board_lines():
+    x_offset = 2
+    y_offset = 2
     pyxel.rect(x_offset, 50 + y_offset, w=150, h=1, col=0)
     pyxel.rect(x_offset, 100 + y_offset, w=150, h=1, col=0)
     pyxel.rect(50 + x_offset, y_offset, h=150, w=1, col=0)
     pyxel.rect(100 + x_offset, y_offset, h=150, w=1, col=0)
     pyxel.rect(x=0, y=156, h=5, w=200, col=0)
+
+def get_transparent_color(value):
+    global selected_value
+
+    if selected_value == value:
+        transparent_color = 5
+    else:
+        transparent_color = 10
+    return transparent_color
+
+def draw_selected_value():
+    global selected_value
+
+    x_offset = 2
+    image_size = 16
     for i in range(9):
-        if selected_value == i + 1:
-            transparent_color = 5
-        else:
-            transparent_color = 10
-        pyxel.blt(x = i*16 + i + x_offset, y=165, img=0, u=0, v = (i+1) * 16, w=image_size, h=image_size, colkey=transparent_color)  
+        transparent_color = get_transparent_color(i+1)
+        pyxel.blt(x = i*image_size + i + x_offset, y=165, img=0, u=0, v = (i+1) * image_size, 
+                  w=image_size, h=image_size, colkey=transparent_color)
+
+def draw():
+    global game_won
+
+    if game_won:
+        pyxel.cls(col=10)
+    draw_cells()
+    draw_board_lines()
+    draw_selected_value()
 
 def get_board_cage(mouse_x, mouse_y):
     return min(int(mouse_x // 20), 8), min(int(mouse_y // 20), 8)
@@ -152,45 +176,52 @@ def board_is_full(board):
             else:
                 return True
             
+def control_quit():
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()
+
+def control_left_click():
+    global cell_coordinates
+    global selected_value
+    
+    mouse_position = (pyxel.mouse_x, pyxel.mouse_y)
+    board_cage = get_board_cage(*mouse_position)
+    if mouse_position[1] < width_size:
+        cell_coordinates = board_cage
+    else:
+        selected_value = board_cage[0] + 1
+        
+def control_right_click():
+    global selected_value
+
+    mouse_position = (pyxel.mouse_x, pyxel.mouse_y)
+    board_cage = get_board_cage(*mouse_position)
+    x, y = board_cage
+    cell_value = puzzle_board[x][y]
+    if cell_value != selected_value:
+        puzzle_board[x][y] = selected_value
+    else:
+        puzzle_board[x][y] = 0
+        
+def control_game_state():
+    global game_won
+
+    if board_is_full(puzzle_board) and board_valid(puzzle_board, solution_board):
+        game_won = True
+    else:
+        game_won = False
+        
 def update():
     global puzzle_board
     global solution_board
-    global cell_coordinates
-    global selected_value
     global game_won
-
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
-    # select the board cage when the player clicks the left mouse button
+    
+    control_quit()
     if pyxel.btnp(0):
-        mouse_position = (pyxel.mouse_x,pyxel.mouse_y)
-        print(mouse_position)
-        board_cage = get_board_cage(*mouse_position)
-        if mouse_position[1] < width_size:
-            cell_coordinates = board_cage
-        else:
-            selected_value = board_cage[0] + 1
-            print('selected value:',selected_value)
-    # update the board cage when the player clicks the right mouse button
-    if pyxel.btnp(1):
-        mouse_position = (pyxel.mouse_x, pyxel.mouse_y)
-        print(mouse_position)
-        board_cage = get_board_cage(*mouse_position)
-        x, y = board_cage
-        cell_value = puzzle_board[x][y]
-        if cell_value != selected_value:
-            puzzle_board[x][y] = selected_value
-        else:
-            puzzle_board[x][y] = 0
-    if board_is_full(puzzle_board):
-        print('full')
-        if board_valid(puzzle_board, solution_board):
-            print('hi')
-            game_won = True
-        else:
-            game_won = False
-    else:
-        game_won = False
+        control_left_click()
+    elif pyxel.btnp(1):
+        control_right_click()
+    control_game_state()
 
 pyxel.run(update, draw)
 print("That was fun, why don't we play again?")
